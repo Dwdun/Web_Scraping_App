c1f0ad0dab7a27e9c60852945e941c835e002bd6
aniinn <anindya.jauzaa.tif425@polban.ac.id>
2026-03-05 17:48:08 +0700

    cleanup unused datetime import

--- Utils (faqih,irfan,anin)/date_filter.py
+++ Utils (faqih,irfan,anin)/date_filter.py
@@ -1,15 +1,14 @@
 import dateparser
-from datetime import datetime  # ini ditambah biar aman
+from datetime import datetime
 
 def parse_date(date_str):
     # parse berbagai format tanggal (indo/inggris)
+    # bisa baca bahasa Indonesia & Inggris
     if not date_str:
         return None
 
     try:
-        # bisa baca bahasa indo & inggris
         result = dateparser.parse(date_str, languages=["id", "en"])
         return result.date() if result else None
-
     except Exception:
         return None
