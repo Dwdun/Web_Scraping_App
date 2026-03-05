6420ce40ecc5c31d595a42cf050b7c2d685f62d6
iirfanr <irfan.ramadhan021006@gmail.com>
2026-03-05 15:37:21 +0700

    tambah file dan console handler

--- Utils (faqih,irfan,anin)/Logger.py
+++ Utils (faqih,irfan,anin)/Logger.py
@@ -3,9 +3,21 @@ import os
 
 os.makedirs("logs", exist_ok=True)
 
-logger = logging.getLogger("my_logger")
+logger = logging.getLogger("scraping_logger")
 logger.setLevel(logging.DEBUG)
 
 formatting = logging.Formatter(
     "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
+
+file_handler = logging.FileHandler("logs/scraper.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatting)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatting)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
