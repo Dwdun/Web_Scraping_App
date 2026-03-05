c2059c1f2c6b0fd22e42f4698c18eab2e06edff0
aniinn <anindya.jauzaa.tif425@polban.ac.id>
2026-03-05 17:05:50 +0700

    error handling

--- Utils (faqih,irfan,anin)/date_filter.py
+++ Utils (faqih,irfan,anin)/date_filter.py
@@ -5,5 +5,10 @@ def parse_date(date_str):
     if not date_str:
         return None
 
-    result = dateparser.parse(date_str, languages=["id", "en"])
-    return result.date() if result else None
+    try:
+        # bisa baca bahasa indo & inggris
+        result = dateparser.parse(date_str, languages=["id", "en"])
+        return result.date() if result else None
+    except Exception:
+        # jika ada error, program tdk crash
+        return None
